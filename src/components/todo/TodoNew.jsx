b3a6d9f75c0e237547465335ab73a0ef00aef80a
NosoaB17 <ngsn176@gmail.com>
2024-10-25 09:51:11 +0700

    Video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,10 +2,24 @@ const TodoNew = (props) => {
   console.log(props);
   const { addNewTodo } = props;
   addNewTodo("Bao");
+
+  const handleClick = () => {
+    alert("me");
+  };
+
+  const handleOnChange = () => {};
+
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={() => {
+          handleOnChange();
+        }}
+      />
+      <button onClick={handleClick} style={{ cursor: "pointer" }}>
+        Add
+      </button>
     </div>
   );
 };
