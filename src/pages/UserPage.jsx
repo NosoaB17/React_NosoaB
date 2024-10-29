9f9ed7e5add99fada5792e5e088babea4fe3d3aa
NosoaB17 <ngsn176@gmail.com>
2024-10-29 11:42:31 +0700

    Video 66

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,5 +1,13 @@
+import UserForm from "../components/user/UserForm";
+import UserTable from "../components/user/UserTable";
+
 const UserPage = () => {
-  return <div>User Page</div>;
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserForm />
+      <UserTable />
+    </div>
+  );
 };
 
 export default UserPage;
